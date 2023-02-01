b19c2f43af660504822f8a05d7ed3e5c401e2a51
OrgulhoPK <106765638+vkamei99@users.noreply.github.com>
2023-02-01 13:58:49 -0300

    Update report.py

--- src/io/report.py
+++ src/io/report.py
@@ -5,5 +5,10 @@ from typing import Dict
 def write_report(path: str, config: Dict, metrics_values) -> None:
     """ escreve o arquivo de relatorio do experimento """
     arquivo = open(path, 'w')
-    arquivo.write(metrics_values)
+    arquivo.write(f'dataset: {config["type"]}\n \
+                  path: {config["train_path"]}\n \
+                  classifier: {config["classifier"]}\n \
+                  training time per sample:{metrics_values}\n \
+                  inference time per sample:{metrics_values}\n \
+                  accuracy:{metrics_values}')
 
