185b712a09c47a5e5c8b370c470f780379cfb31e
OrgulhoPK <106765638+vkamei99@users.noreply.github.com>
2023-02-01 13:58:36 -0300

    Formatação do report

--- main.py
+++ main.py
@@ -3,15 +3,21 @@ from src.datasets.dataset_factory import create_dataset
 from src.classifiers.classifier_factory import create_classifier
 from src.experiment import Experiment
 from src.io.args import parse_args
+import argparse
 from src.io.config import load_config
 from src.io.report import write_report
 
 
 def main():
     # obter os nomes dos arquivos de configuracao e de saida da linha de comando
-    args = parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("config_path", type= str)
+
+    parser.add_argument("report_path", type= str)
+    
+    argumentos = parser.parse_args()
     # le o arquivo json e retorna como um dicionario
-    config = load_config(args.config_path)
+    config = load_config(argumentos.config_path)
 
     train_dataset = create_dataset(config["train_path"], config["type"])
     test_dataset = create_dataset(config["test_path"], config["type"])
@@ -21,7 +27,7 @@ def main():
     metrics = experiment.run(classifier)
 
     # escreve o arquivo de saida
-    write_report(args.report_path, config, metrics)
+    write_report(argumentos.report_path, config, metrics)
 
     print("Success.")
 
--- src/io/report.py
+++ src/io/report.py
@@ -4,3 +4,6 @@ from typing import Dict
 
 def write_report(path: str, config: Dict, metrics_values) -> None:
     """ escreve o arquivo de relatorio do experimento """
+    arquivo = open(path, 'w')
+    arquivo.write(metrics_values)
+
