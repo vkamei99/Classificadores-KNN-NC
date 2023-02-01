147848b4d31d7fb93ecf069111f4351833781f22
OrgulhoPK <106765638+vkamei99@users.noreply.github.com>
2023-02-01 14:12:03 -0300

    calculo da acurácia

--- src/metrics.py
+++ src/metrics.py
@@ -4,4 +4,15 @@ from typing import List
 
 def accuracy(true_classes: List[str], predicted_classes: List) -> float:
     """  calcula o percentual de acerto """
-    return 0
+    #criar gabarito (true_classes)
+    #comparar gabarito com o treino (predicted_classes)
+    quantidade = 0 #quantidade de vetores usados no treino
+    acertos = 0
+
+    for i in range(quantidade):
+        if true_classes[i] == predicted_classes[i]:
+            acertos += 1
+    
+    acurracy = (acertos*100)/quantidade
+
+    return accuracy
