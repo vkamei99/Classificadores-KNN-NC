3a53093e847fe77416d5f192017e07c230183ff5
vkamei99 <106765638+vkamei99@users.noreply.github.com>
2023-02-03 00:05:58 -0300

    Update config.py

--- src/io/config.py
+++ src/io/config.py
@@ -7,3 +7,15 @@ def load_config(path: str) -> Dict:
         config_dict = json.load(f)
     #abre o arquivo json e o salva em "f" depois fecha o arquivo sozinho
     return config_dict
+
+"""
+Este código é uma função chamada load_config que lê um arquivo JSON e o retorna como um dicionário em Python.
+
+A função recebe como parâmetro o caminho do arquivo (path: str) e retorna o dicionário (Dict).
+
+O arquivo é aberto com o comando "with open (path, 'r') as f". Aqui, "f" é uma variável que será usada para referenciar o arquivo aberto.
+
+Em seguida, usamos a função "json.load (f)" para ler o conteúdo do arquivo e armazená-lo como um dicionário em "config_dict".
+
+Finalmente, a função retorna "config_dict" como resultado.
+"""
