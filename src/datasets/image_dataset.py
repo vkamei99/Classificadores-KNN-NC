edf6d4039cfdaf90e01ee2148b9534215c67c072
vkamei99 <106765638+vkamei99@users.noreply.github.com>
2023-02-03 01:26:18 -0300

    Terminado o metodo construtor e o size
    construtor:  ler arquivo contendo os nomes das imagens e as classes e armazenar em uma lista
    
    size: retorna o número de amostras no dataset

--- src/datasets/image_dataset.py
+++ src/datasets/image_dataset.py
@@ -1,7 +1,5 @@
-
 from typing import Tuple, Any, Dict
 from .dataset_interface import DatasetInterface
-
 
 class ImageDataset(DatasetInterface):
     def __init__(self, path: str) -> None:
@@ -10,12 +8,25 @@ class ImageDataset(DatasetInterface):
         self.path = path
         self.image_name = []
         self.image_class = []
+        
+        with open(path, 'r') as f:
+            for line in f:
+                image_name, image_class = line.split()
+                self.image_name.append(image_name)
+                self.image_class.append(image_class)
 
     def size(self) -> int:
         # retornar tamanho do dataset (numero de linhas do arquivo)
-        return 0
+        return len(self.image_name)
 
     def get(self, idx: int) -> Tuple[Any, str]:
         # ler a i-esima imagem do disco usando a biblioteca cv2 e retorna a imagem e a respectiva classe
         return 0, ""
 
+'''
+A interface de datasets define
+que todos os datasets devem implementar os métodos size e get, sendo que o
+primeiro retorna o número de amostras no dataset (número de imagens ou
+notícias) e o segundo retorna o i-ésimo elemento em formato vetorial e a
+respectiva classe.
+'''
