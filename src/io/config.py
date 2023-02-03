74b921030b9e47628e4bae3e74628335cc1a63e3
vkamei99 <106765638+vkamei99@users.noreply.github.com>
2023-02-02 23:56:59 -0300

    Le os arquivos .json e os salva como um dicionario

--- src/io/config.py
+++ src/io/config.py
@@ -1,24 +1,9 @@
-
-
 from typing import Dict
-
+import json
 
 def load_config(path: str) -> Dict:
     """ le o arquivo json e retorna como um dicionario """
-
-    # we're returning dummy values just to make the main program runnable
-    dummy_dict = {
-        "train_dataset": {
-            "type": "image",
-            "path": "data/datasets/img_small/train.txt"
-        },
-        "test_dataset": {
-            "type": "image",
-            "path": "data/datasets/img_small/test.txt"
-        },
-        "classifier": {
-            "type": "knn"
-        }
-    }
-
-    return dummy_dict
+    with open(path, 'r') as f:
+        config_dict = json.load(f)
+    #abre o arquivo json e o salva em "f" depois fecha o arquivo sozinho
+    return config_dict
