447d643d32c8c578f4ddc8af550fc2a4baa14ce4
vkamei99 <106765638+vkamei99@users.noreply.github.com>
2023-02-03 00:33:03 -0300

    Update dataset_factory.py

--- src/datasets/dataset_factory.py
+++ src/datasets/dataset_factory.py
@@ -12,3 +12,10 @@ def create_dataset(path: str, type: str) -> DatasetInterface:
         return NewsDataset(path)
     else:
         raise Exception("Dataset type not found.")
+
+"""
+Essa função cria um objeto do tipo Dataset a partir dos argumentos "path" e "type".
+Se o "type" for "image", a função retorna uma instância da classe ImageDataset, inicializada com "path".
+Se o "type" for "news", a função retorna uma instância da classe NewsDataset, inicializada com "path".
+Se o "type" não for encontrado, a função levanta uma exceção informando que o tipo de dataset não foi encontrado.
+"""
