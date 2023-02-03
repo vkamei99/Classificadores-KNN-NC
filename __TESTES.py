76d6566957e50d8ac9602dad0b9989f493d48787
vkamei99 <106765638+vkamei99@users.noreply.github.com>
2023-02-03 00:05:11 -0300

    teste da leitura do config.json

--- __TESTES.py
+++ __TESTES.py
@@ -1,17 +1,21 @@
-dicionario = {"chave":"valor"}
+from src.datasets.dataset_factory import create_dataset
+from src.classifiers.classifier_factory import create_classifier
+from src.experiment import Experiment
+from src.io.args import parse_args
+import argparse
+from src.io.config import load_config
+from src.io.report import write_report
 
-dummy_dict = {
-        "train_dataset": {
-            "type": "image",
-            "path": "data/datasets/img_small/train.txt"
-        },
-        "test_dataset": {
-            "type": "image",
-            "path": "data/datasets/img_small/test.txt"
-        },
-        "classifier": {
-            "type": "knn"
-        }
-    }
+def main():
+    # obter os nomes dos arquivos de configuracao e de saida da linha de comando
+    parser = argparse.ArgumentParser()
+    parser.add_argument("config_path", type= str)#arquivo de configuração
+    parser.add_argument("report_path", type= str)#arquivo de saida da linha de comando
+    argumentos = parser.parse_args()
+    
+    # le o arquivo json e retorna como um dicionario
+    config = load_config(argumentos.config_path)
+    print(config['train_path'])
 
-print(dummy_dict["train_dataset"]['path'])
+if __name__ == "__main__":
+    main()
