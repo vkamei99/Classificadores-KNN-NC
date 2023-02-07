b6d81b34b73ff3550739021c2801fbfcb72c7391
vkamei99 <106765638+vkamei99@users.noreply.github.com>
2023-02-07 00:32:42 -0300

    ve os k mais proximos

--- src/classifiers/knn_classifier.py
+++ src/classifiers/knn_classifier.py
@@ -29,12 +29,24 @@ class KnnClassifier(ClassifierInterface):
         for i in range(self.tam_test):
             self.test_amostras.append(test_dataset.get(i))
 
+        #calcula a distancia de todos os vetores
         for i in range(self.tam_test):
             for j in range(self.tam_train):
                 somatorio = 0
-                for x in range(len(self.test_amostras[0][0])):
+                for x in range(len(self.test_amostras[i][0])):
                     somatorio += (self.test_amostras[i][0][x] - self.train_amostras[j][0][x]) ** 2
                 dist = (somatorio)**1/2
                 distancia.append(dist)
-                           
+        
+        #ve os 5 pontos com menor distancia
+        menor =[(distancia[0], 0)]
+        for i in range(k):
+            for j in range(len(distancia)):
+                if distancia[j] < menor[i][0]:
+                    menor.append((distancia[j], j))
+                    distancia.pop(j)
+
+        
+
+
         return []
