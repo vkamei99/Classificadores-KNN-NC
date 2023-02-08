e3502b48599de5311bab0d515b052343d9fb1113
Viktor Kamei Mota <106765638+vkamei99@users.noreply.github.com>
2023-02-08 12:41:30 -0300

    Update debug.py

--- debug.py
+++ debug.py
@@ -6,20 +6,17 @@ import argparse
 from src.io.config import load_config
 from src.io.report import write_report
 
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("config_path", type= str)
-    parser.add_argument("report_path", type= str)
+    parser.add_argument("config_path", type=str)
+    parser.add_argument("report_path", type=str)
     argumentos = parser.parse_args()
-    
+
     config = load_config(argumentos.config_path)
     train_dataset = create_dataset(config["train_path"], config["type"])
-    test_dataset = create_dataset(config["test_path"], config["type"])
-
-    experiment = Experiment(train_dataset, test_dataset)
-    metrics = experiment.run(classifier)
+    train_dataset.get(1)
 
 
-    
 if __name__ == "__main__":
     main()
