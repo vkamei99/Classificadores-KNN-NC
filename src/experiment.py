14bfcbae6ac260188eb1563008da13a44307b7f6
Viktor Kamei Mota <106765638+vkamei99@users.noreply.github.com>
2023-02-08 12:56:13 -0300

    Update experiment.py

--- src/experiment.py
+++ src/experiment.py
@@ -3,6 +3,7 @@ from typing import Union, Dict, List
 from src.datasets.dataset_interface import DatasetInterface
 from src.classifiers.classifier_interface import ClassifierInterface
 from src.metrics import accuracy
+import time
 
 
 class Experiment:
@@ -16,11 +17,18 @@ class Experiment:
 
     def run(self, classifier: ClassifierInterface) -> Dict[str, float]:
         """ executa o experimento """
+        treino_t = time.time()
         classifier.train(self.train_dataset)
+        treino_to = time.time()
+
+        predicit_t = time.time()
         pred_classes = classifier.predict(self.test_dataset)
+        predicit_to = time.time()
 
         metrics = {
             "accuracy": accuracy(self.true_classes, pred_classes)
+            "training_time_per_sample": (treino_to - treino_t)/self.test_dataset.size()
+            "inference_time_per_sample": (predicit_to-predicit_t)/self.train_dataset.size()
         }
 
         return metrics
