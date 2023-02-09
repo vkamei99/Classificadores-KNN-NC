43f871584bc63ca5b5e7c2f9be82ecc5663eaa57
vkamei99 <106765638+vkamei99@users.noreply.github.com>
2023-02-08 21:30:59 -0300

    Update metrics.py

--- src/metrics.py
+++ src/metrics.py
@@ -4,8 +4,6 @@ from typing import List
 
 def accuracy(true_classes: List[str], predicted_classes: List) -> float:
     """  calcula o percentual de acerto """
-    #criar gabarito (true_classes)
-    #comparar gabarito com o treino (predicted_classes)
     quantidade = len(true_classes) #quantidade de vetores usados no treino
     acertos = 0
 
